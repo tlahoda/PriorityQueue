c626f52962f538bc9ddae9c9ebcad3eb935292e6
Thomas Lahoda <tlahoda@gmail.com>
2011-09-14 15:55:35 -0400

    Refactored to using a vector of maps of lists instead of a map of maps of lists. This improves push's complexity to O(log p) and degrades pop's complexity to O(k).

--- PriorityQueue.h
+++ PriorityQueue.h
@@ -26,6 +26,7 @@
 #define DATA_PRIORITYQUEUE_H
 
 #include <string>
+#include <vector>
 #include <map>
 #include <list>
 #include <algorithm>
@@ -34,70 +35,6 @@
 
 namespace data {
   using namespace std;
-
-  /**
-   * \struct Min
-   * Makes lower values the higher priorities.
-   */
-  struct Min {
-    /**
-     * Determines if the priority is lower than min.
-     *
-     * \param priority The priority to check.
-     * \param min The min to check.
-     *
-     * \return bool True if lower, false otherwise.
-     */
-    static bool compare (const string& priority, const string& min) {
-      return priority < min;
-    };
-
-    /**
-     * Get the iterator from the container.
-     *
-     * \tparam T The type of the container.
-     *
-     * \param t The container.
-     *
-     * \return The iterator.
-     */
-    template<typename T>
-    static typename T::iterator iterator (T& t) {
-      return t.begin ();
-    };
-  }; //Min
-
-  /**
-   * \struct Max
-   * Makes higher values the higher priorities.
-   */
-  struct Max {
-    /**
-     * Determines if the priority is higher than max.
-     *
-     * \param priority The priority to check.
-     * \param max The max to check.
-     *
-     * \return bool True if higher, false otherwise.
-     */
-    static bool compare (const string& priority, const string& max) {
-      return priority > max;
-    };
-
-    /**
-     * Get the iterator from the container.
-     *
-     * \tparam T The type of the container.
-     *
-     * \param t The container.
-     *
-     * \return The iterator.
-     */
-    template<typename T>
-    static typename T::iterator iterator (T& t) {
-      return --t.end ();
-    };
-  }; //Max
 
   /**
    * \struct PriorityQueue
@@ -112,18 +49,21 @@ namespace data {
      * \typedef map<string, list<T> > BUCKET
      * The bucket type.
      */
-    typedef map<string, list<T> > BUCKET;
+    typedef map<string, list<T>> BUCKET;
 
     /**
-     * \typedef map<unsigned int, BUCKET> BUCKETS
+     * \typedef vector<unsigned int, BUCKET> BUCKETS
      * The type of the buckets container.
      */
-    typedef map<unsigned int, BUCKET> BUCKETS;
+    typedef vector<BUCKET> BUCKETS;
 
     /**
-     * Constructs a PriorityQueue. This is a constant time operation.
+     * Constructs a PriorityQueue.
+     *
+     * \param maxKeyLength The maximum number of digits a key will have.
      */
-    PriorityQueue () : buckets_ (), size_ (0), curDigitsBucket_ (), curPriorityBucket_ (), curHighest_ () {};
+    PriorityQueue (unsigned int maxKeyLength) 
+      : buckets_ (maxKeyLength), size_ (0), curPriorityBucket_ (), curHighest_ () {};
 
     /**
      * Destroys a PriorityQueue. Complexity is O(n) where n is the number of elements
@@ -132,18 +72,9 @@ namespace data {
     ~PriorityQueue () {};
 
     /**
-     * Pushes an element onto the PriorityQueue. push has a typical complexity of
-     * O(log k*p) where k is the maximum number of digits a priority may have and
-     * p is the number of unigue  priorities currently in the queue having the
-     * same length as the priority being pushed. Worst case complexity is
-     * O(log 2*k*Np) where k is the maximum number of digits a priority may have
-     * and Np is the size of the set of priorities having the same number of
-     * digits as the priority being pushed. This occurs when both a digits bucket
-     * has to be created and a priority bucket has to be created. This is 
-     * guaranteed to occur the first time a priority is pushed into the queue.
-     *
-     * Any issue with construction cost of the maps or the list could be
-     * alleviated by caching the maps instead of constructing and destroying them.
+     * Pushes an element onto the PriorityQueue. push has a complexity of O(log p)
+     * where p is the number of unigue  priorities currently in the queue having
+     * the same length as the priority being pushed.
      *
      * \param priority The priority of the element.
      * \param t The element to insert.
@@ -151,19 +82,10 @@ namespace data {
      * \return void.
      */
     void push (const string& priority, const T& t) {
-      unsigned int numDigits = priority.size ();
-      auto digitsBucket = buckets_.find (numDigits);
-      if (digitsBucket == buckets_.end ())
-        digitsBucket = buckets_.insert (make_pair (numDigits, BUCKET ())).first;
-     
-      auto priorityBucket = digitsBucket->second.find (priority);
-      if (priorityBucket == digitsBucket->second.end ())
-        priorityBucket = digitsBucket->second.insert (make_pair (priority, list<T> ())).first;
-
+      auto priorityBucket = buckets_[priority.size () - 1].insert (make_pair (priority, list<T> ())).first;
       priorityBucket->second.push_back (t);
 
       if (size_ == 0 || DIRECTION::compare (priority, *curHighest_)) {
-        curDigitsBucket_ = digitsBucket;
         curPriorityBucket_ = priorityBucket;
         curHighest_ = priorityBucket->second.begin ();
       }
@@ -171,10 +93,8 @@ namespace data {
     };
 
     /**
-     * Removes and returns the highest priority element. This is a constant time operation.
-     * This is acheived by the removal of empty buckets from the structure, by using C++'s
-     * begin/end iterators to find the highest priority, and by caching the current highest
-     * priority element, the priority bucket it is in and the digits bucket it is in.
+     * Removes and returns the highest priority element. pop has a complexity of
+     * O(k) where k is the maximum number of digits a priority will have.
      *
      * \return The highest priority element.
      *
@@ -182,20 +102,21 @@ namespace data {
      */
     T pop () {
       if (size_ == 0) throw runtime_error ("Priority queue is empty.");
-
+      
       T t = *curHighest_;
       curPriorityBucket_->second.erase (curHighest_);
       if (curPriorityBucket_->second.empty ())
-        curDigitsBucket_->second.erase (curPriorityBucket_);
-      
-      if (curDigitsBucket_->second.empty ())
-        buckets_.erase (curDigitsBucket_);
+        buckets_[curPriorityBucket_->first.size () - 1].erase (curPriorityBucket_);
       --size_;
 
       if (size_ != 0) {
-        curDigitsBucket_ = DIRECTION::iterator (buckets_);
-        curPriorityBucket_ = DIRECTION::iterator (curDigitsBucket_->second);
-        curHighest_ = curPriorityBucket_->second.begin ();
+        for (auto cur = DIRECTION::begin (buckets_), end = DIRECTION::end (buckets_); cur != end; ++cur) {
+          if (!cur->empty ()) {
+            curPriorityBucket_ = DIRECTION::convert (DIRECTION::begin (*cur));
+            curHighest_ = curPriorityBucket_->second.begin ();
+            break;
+          }
+        }
       }
       return t;
     };
@@ -224,14 +145,13 @@ namespace data {
      */
     list<T> pop_all () {
       list<T> res;
-      while (!buckets_.empty ()) {
-        auto curDigitsBucket = DIRECTION::iterator (buckets_);
-        while (!curDigitsBucket->second.empty ()) {
-          auto curPriorityBucket = DIRECTION::iterator (curDigitsBucket->second);
+      for (auto curDigitsBucket = DIRECTION::begin (buckets_), end = DIRECTION::end (buckets_);
+        curDigitsBucket != end; ++curDigitsBucket) {
+        while (!curDigitsBucket->empty ()) {
+          auto curPriorityBucket = DIRECTION::begin (*curDigitsBucket);
           res.splice (res.end (), curPriorityBucket->second);
-          curDigitsBucket->second.erase (curPriorityBucket);
+          curDigitsBucket->erase (DIRECTION::convert (curPriorityBucket));
         }
-        buckets_.erase (curDigitsBucket);
       }
       size_ = 0;
       return res;   
@@ -252,23 +172,26 @@ namespace data {
     unsigned int size () const { return size_; };
 
     /**
-     * Prints the contents of the queue.
+     * Writes the PriorityQueue to an ostream.
      *
-     * \return void.
+     * \param os The ostream to which to write.
+     * \param queue The PriorityQueue to write.
+     *
+     * \return A reference to the ostream.
      */
-    void print () const {
-      for (auto curDigitsBucket = buckets_.begin (), end = buckets_.end (); 
-           curDigitsBucket != end; ++curDigitsBucket) {
-        cout << curDigitsBucket->first << endl;
-        for (auto curPriorityBucket = curDigitsBucket->second.begin (), endBucket = curDigitsBucket->second.end ();
-             curPriorityBucket != endBucket; ++curPriorityBucket) {
-          cout << "\t" << curPriorityBucket->first << endl;
-          for (auto cur = curPriorityBucket->second.begin (), endEle = curPriorityBucket->second.end ();
-              cur != endEle; ++cur) {
-            cout << "\t\t" << *cur << endl;
+    friend ostream& operator<< (ostream& os, const PriorityQueue& queue) {
+      int i = 0;
+      for (auto curDigitsBucket = queue.buckets_.begin (), end = queue.buckets_.end (); curDigitsBucket != end; ++curDigitsBucket) {
+        os << "  digits - " << i++ << endl;
+        for (auto curPriorityBucket = curDigitsBucket->begin (), endBucket = curDigitsBucket->end (); curPriorityBucket != endBucket; ++curPriorityBucket) {
+          os << "    priority - " << curPriorityBucket->first << endl;
+          for (auto cur = curPriorityBucket->second.begin (), endEle = curPriorityBucket->second.end (); cur != endEle; ++cur) {
+            os << "      element - " << *cur << endl;
           }
         }
+        os << endl;
       }
+      return os;
     };
 
     private:
@@ -299,11 +222,6 @@ namespace data {
       int size_;
 
       /**
-       * The digits bucket in which the current highest priority element resides.
-       */
-      typename BUCKETS::iterator curDigitsBucket_;
-
-      /**
        * The priority bucket in which the current highest priority element resides.
        */
       typename BUCKET::iterator curPriorityBucket_;
@@ -315,6 +233,126 @@ namespace data {
   }; //PriorityQueue
 
   /**
+   * \struct Min
+   * Makes lower values the higher priorities.
+   */
+  struct Min {
+    /**
+     * Determines if the priority is lower than min.
+     *
+     * \param priority The priority to check.
+     * \param min The min to check.
+     *
+     * \return bool True if lower, false otherwise.
+     */
+    static bool compare (const string& priority, const string& min) {
+      return priority < min;
+    };
+
+    /**
+     * Get the begin iterator from the container.
+     *
+     * \tparam T The type of the container.
+     *
+     * \param t The container.
+     *
+     * \return The iterator.
+     */
+    template<typename T>
+    static typename T::iterator begin (T& t) {
+      return t.begin ();
+    };
+
+    /**
+     * Get the end iterator from the container.
+     *
+     * \tparam T The type of the container.
+     *
+     * \param t The container.
+     *
+     * \return The iterator.
+     */
+    template<typename T>
+    static typename T::iterator end (T& t) {
+      return t.end ();
+    };
+
+    /**
+     * Ensures the iterator is a forward_iterator.
+     *
+     * \tparam T The type of the iterator.
+     *
+     * \param t The iterator.
+     *
+     * \return The converted iterator.
+     */
+    template<typename T>
+    static T convert (const T& t) {
+      return t;
+    };
+  }; //Min
+
+  /**
+   * \struct Max
+   * Makes higher values the higher priorities.
+   */
+  struct Max {
+    /**
+     * Determines if the priority is higher than max.
+     *
+     * \param priority The priority to check.
+     * \param max The max to check.
+     *
+     * \return bool True if higher, false otherwise.
+     */
+    static bool compare (const string& priority, const string& max) {
+      return priority > max;
+    };
+
+    /**
+     * Get the begin iterator from the container.
+     *
+     * \tparam T The type of the container.
+     *
+     * \param t The container.
+     *
+     * \return The iterator.
+     */
+    template<typename T>
+    static typename T::reverse_iterator begin (T& t) {
+      return t.rbegin ();;
+    };
+
+    /**
+     * Get the end iterator from the container.
+     *
+     * \tparam T The type of the container.
+     *
+     * \param t The container.
+     *
+     * \return The iterator.
+     */
+    template<typename T>
+    static typename T::reverse_iterator end (T& t) {
+      return t.rend ();
+    };
+
+    /**
+     * Ensures the iterator is a forward_iterator.
+     *
+     * \tparam T The type of the iterator.
+     *
+     * \param t The iterator.
+     *
+     * \return The converted iterator.
+     */
+    template<typename T>
+    static typename T::iterator_type convert (const T& t) {
+      return --t.base ();
+    };
+  }; //Max
+
+  /**
    * \struct MinPriorityQueue
    * Lower valued priorities are higher priorities. This exists as a class because
    * g++ does not yet have templated typedefs.
@@ -322,7 +360,9 @@ namespace data {
    * \tparam T The type to be held by the queue.
    */
   template<typename T>
-  struct MinPriorityQueue : public PriorityQueue<T, Min> {};
+  struct MinPriorityQueue : public PriorityQueue<T, Min> {
+    MinPriorityQueue (unsigned int maxKeyLength) : PriorityQueue<T, Min> (maxKeyLength) {};
+  };
 
   /**
    * \struct MaxPriorityQueue
@@ -332,7 +372,9 @@ namespace data {
    * \tparam T The type to be held by the queue.
    */
   template<typename T>
-  struct MaxPriorityQueue : public PriorityQueue<T, Max> {};
+  struct MaxPriorityQueue : public PriorityQueue<T, Max> {
+    MaxPriorityQueue (unsigned int maxKeyLength) : PriorityQueue<T, Max> (maxKeyLength) {};
+  };
 }; //data
 
 #endif //DATA_PRIORITYQUEUE_H
